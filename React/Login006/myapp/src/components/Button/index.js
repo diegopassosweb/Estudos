b2f3bd1e0dc8630feede92e095d978dd1dd845d4
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-12-01 08:58:03 -0300

    delete

---
+++ React/Login006/myapp/src/components/Button/index.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import * as C from "./styles";
+
+const Button = ({Text, onClick, type ="button"}) => {
+  return (
+    <C.Button type={type} onClick={onClick}>
+        {Text}
+    </C.Button>
+  )
+};
+
+export default Button;
