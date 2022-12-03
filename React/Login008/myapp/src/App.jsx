d10eea788c2fb76de5b7b067f259445df115343c
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-12-03 09:10:14 -0300

    login v08

---
+++ React/Login008/myapp/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import AppRoutes from "./AppRoutes";
+
+function App() {
+  return (
+    <div className="App">
+       <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
