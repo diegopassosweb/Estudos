462062cdad68527bd2b1673e8816702be62c637c
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-12-06 11:19:15 -0300

    login v09

---
+++ React/Login009/myapp/src/AppRoutes.jsx
@@ -0,0 +1,37 @@
+
+import { useState } from "react";
+import { BrowserRouter as Router, Route,Routes, Navigate} from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import { AuthContext, AuthProvider } from "./contexts/auth";
+import { useContext } from "react";
+
+const AppRoutes = () => {   
+
+   const Private = ({children}) => {
+    const {authenticated, loading} = useContext(AuthContext);
+
+    if(loading) {
+        return <div className="loading">Carregando...</div>
+    }
+    
+    if(!authenticated) {
+        return <Navigate to="/login" />;
+    }
+
+    return children;
+   }
+    return (
+        <Router>
+            <AuthProvider>
+                <Routes>
+                <Route exact path="/login" element={<LoginPage />}/>
+                <Route exact path="/" element={<Private><HomePage /> </Private>}/>
+                </Routes>  
+            </AuthProvider>
+        </Router>
+    )
+}
+
+export default AppRoutes;
---
+++ React/Login009/myapp/src/contexts/auth.jsx
@@ -0,0 +1,54 @@
+
+import { useState } from "react";
+import { useEffect } from "react";
+import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AuthContext = createContext();
+
+
+export const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const recoveredUser = localStorage.getItem("user");
+
+        if(recoveredUser) {
+            setUser(JSON.parse(recoveredUser));
+        }
+
+        setLoading(false);
+    },[]);
+
+    const login = (email, password) => {
+        console.log("login auth", {email, password});
+
+        const loggedUser = {
+            id: "123",
+            email,
+        }
+        localStorage.setItem("user", JSON.stringify(loggedUser));
+
+        if(password === "secret") {
+            setUser(loggedUser);
+            navigate("/");
+        }
+    };
+
+
+    const logout = () => {
+        console.log("logout");
+        localStorage.removeItem("user");
+        setUser(null);
+        navigate("/login");
+    };
+
+
+    return (
+    <AuthContext.Provider value={{authenticated: !!user, user, login, logout, loading}}>
+        {children}
+    </AuthContext.Provider>
+    )
+}
