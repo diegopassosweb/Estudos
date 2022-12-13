f860d377e042116ce7f0704a215e6675e3be383a
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-12-13 06:38:03 -0300

    login v14

---
+++ React/Login014/myapp/src/pages/HomePage/index.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/auth";
+
+const HomePage = () => {
+    const {authenticated,logout} = useContext(AuthContext);
+
+    const handleLogout = () => {
+        logout()
+    }
+  return <div>
+    <h1>HomePage</h1>
+    <p>{String(authenticated)}</p>
+    <button onClick={handleLogout}>Logout</button>
+    </div>;
+};
+
+export default HomePage;
