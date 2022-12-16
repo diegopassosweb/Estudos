9e12ad07dca547261dc60697cb23ff1d7270ac96
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-12-16 16:00:48 -0300

    login v17

---
+++ React/Login017/myapp/src/AppRoutes.jsx
@@ -0,0 +1,35 @@
+import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import { AuthContext, AuthProvider } from "./contexts/auth";
+import { useContext } from "react";
+
+
+export const AppRoutes = () => {
+
+    const Private = ({children}) => {
+        const {authenticated, loading} = useContext(AuthContext);
+
+        if(loading) {
+            return <div id="loading">Carregando...</div>
+        }
+
+        if(!authenticated) {
+            return <Navigate to="/login" />;
+        }
+
+        return children
+    };
+  
+    return (
+        <Router>
+        <AuthProvider>
+        <Routes>
+                <Route exact path="/login" element={<LoginPage />} />
+                <Route exact path="/" element={<Private> <HomePage /></Private>} />
+        </Routes>
+        </AuthProvider>
+        </Router>
+       
+    )
+}
---
+++ React/Login017/myapp/src/pages/HomePage/index.jsx
@@ -0,0 +1,18 @@
+
+import React, { useContext } from "react";
+import { AuthContext } from "../../contexts/auth";
+
+const HomePage = () => {
+  const {authenticated, logout} = useContext(AuthContext)
+
+  const handleLogout = () => {
+    logout();
+  }
+  return <div>
+    <h1>HomePage</h1>
+    <p>{String(authenticated)}</p>
+    <button onClick={handleLogout}>Logout</button>
+  </div>;
+};
+
+export default HomePage;
