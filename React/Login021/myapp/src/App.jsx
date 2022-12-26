f5150c1e441d066c7a99d8cfcb77e19bfd8165ce
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-12-26 03:02:18 -0300

    login 021

---
+++ React/Login021/myapp/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import AppRoutes from './AppRoutes';
+
+
+const App = () => {
+  return (
+    <div className="App">
+     <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Login021/myapp/src/AppRoutes.jsx
@@ -0,0 +1,38 @@
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+
+import React from "react";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import {AuthContext, AuthProvider } from "./contexts/auth";
+import { useContext } from "react";
+
+
+const AppRoutes = () => {
+  const Private = ({children}) => {
+    const {authenticated, loading} = useContext(AuthContext);
+
+    if(loading) {
+      return <div className="loading">Carregando...</div>
+    }
+
+    if(!authenticated) {
+      return <Navigate to="/login" />;
+    }
+      return children;
+  }
+
+  return (
+      <Router>
+      <AuthProvider>
+         <Routes>
+            <Route exact path="/login" element={<LoginPage />} />
+            <Route exact path="/" element={<Private> <HomePage /></Private>} />
+        </Routes>
+      </AuthProvider>
+       
+    </Router>
+    
+  )
+};
+
+export default AppRoutes;
