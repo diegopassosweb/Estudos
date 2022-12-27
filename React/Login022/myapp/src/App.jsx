e6aed02b1580bb1785926669a9ee7965400b2b50
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-12-27 11:37:08 -0300

    login v22

---
+++ React/Login022/myapp/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { AppRoutes } from './AppRoutes';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+  return (
+    <div className="App">
+      <AppRoutes />
+    </div>
+    
+  );
+}
+
+export default App;
