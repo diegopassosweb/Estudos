268e5fbecae6feae61386028a581bb291e3ee7ca
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-12-30 03:33:26 -0300

    login v025

---
+++ React/Login025/myapp/src/pages/LoginPage/index.jsx
@@ -0,0 +1,42 @@
+
+import React from "react";
+import { useContext } from "react";
+import { useState } from "react";
+import { AuthContext } from "../../contexts/auth";
+
+const LoginPage = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const {authenticated, login} = useContext(AuthContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("submit", {email, password});
+
+        login(email, password);
+    }
+
+  return (
+    <div id="login">
+        <h1 className="title">Login do sistema</h1>
+        <p>{String(authenticated)}</p>
+        <form action="" className="form" onSubmit={handleSubmit}>
+            <div className="field">
+                <label htmlFor="">Email</label>
+                <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+            </div>
+
+            <div className="field">
+                <label htmlFor="">Senha</label>
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            </div>
+            <div className="actions">
+                <button type="submit">Entrar</button>
+            </div>
+        </form>
+    </div>
+  )
+};
+
+export default LoginPage;
