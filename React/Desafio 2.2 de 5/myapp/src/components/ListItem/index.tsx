d694bae37ebdcbb1f9fff68b9a079fe6110ebcd1
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2023-01-20 10:54:24 -0300

    desafio 2.2 de 5

---
+++ React/Desafio 2.2 de 5/myapp/src/components/ListItem/index.tsx
@@ -0,0 +1,23 @@
+
+import React, { useState } from "react";
+import { Item } from "../../types/Item";
+import * as C from "./styles";
+
+type Props = {
+    item: Item
+}
+
+const ListItem = ({item}: Props) => {
+    const [isChecked, setisChecked] = useState(item.done);
+
+  return (
+    <C.Container done={isChecked}>
+        <input type="checkbox" checked={isChecked}
+        onChange={e => setisChecked(e.target.checked)}
+        />
+        <label htmlFor="">{item.name}</label>
+    </C.Container>
+  )
+};
+
+export default ListItem;
