548a3887d76b3faf238d6bda1dc21cd2e18b5aa3
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2023-02-02 09:09:01 -0300

    delete

---
+++ React/Calculadora 02/myapp/src/operationButton.jsx
@@ -0,0 +1,7 @@
+import { ACTIONS } from "./App";
+
+export default function operationButton ({dispatch, operation}) {
+    return <button onClick={() => dispatch({type: ACTIONS.CHOOSE_OPERATION, payload: {operation} })}>
+        {operation}
+        </button>
+}
