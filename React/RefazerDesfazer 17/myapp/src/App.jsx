1402b05b0b1373016a67da4ba3f03abb1aa1db35
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2023-03-28 02:02:25 -0300

    RedoUndo 17

---
+++ React/RefazerDesfazer 17/myapp/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [list, setList] = useState([]);
+  const [undid, setUndid] = useState([]);
+
+  const handleClick = (e) => {
+    const newDot = {
+      clientX: e.clientX,
+      clientY: e.clientY,
+    }
+    setList((prev) => [...prev, newDot]);
+    setUndid([]);
+  }
+
+  const handleUndo = (e) => {
+    e.stopPropagation();
+
+    if(list.length === 0) {
+      return;
+    }
+
+    const lasItem = list[list.length -1];
+    setUndid((prev) => [...prev, lasItem]);
+
+    setList((prev) => {
+      const newArr = [...prev].slice(0, -1);
+      return newArr;
+    })
+  }
+
+  const handleRedo = (e) => {
+    e.stopPropagation();
+
+    if(undid.length === 0) {
+      return;
+    }
+
+    const recoveredDot = undid[undid.length -1];
+
+    setUndid((prev) => {
+      const newArr = [...prev].slice(0, -1);
+      return newArr;
+    });
+    setList((prev) => [...prev, recoveredDot]);
+  }
+
+  return (
+    <div id="page" onClick={handleClick}>
+      <button onClick={handleUndo}>Desfazer</button>
+      <button onClick={handleRedo}>Refazer</button>
+      {list.map((item, index) => (
+        <span className="dot" key={index} style={{left: item.clientX, top: item.clientY}} />
+      ))}
+     
+    </div>
+  )
+}
+
+export default App
