a02ae52930c7833f531b169e69f8a9dd25615795
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2023-07-16 02:20:34 -0300

    Reach To Do List 2

--- React/To Do List 02/myapp/src/components/TodoForm.jsx
+++ React/To Do List 02/myapp/src/components/TodoForm.jsx
@@ -1,14 +1,27 @@
 
-import React from 'react'
+import {useState} from 'react'
 
-const TodoForm = () => {
+const TodoForm = ({addTodo}) => {
+
+  const [value, setValue] = useState('');
+  const [category, setCategory] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(!value || !category) return;
+    addTodo(value, category)
+    setValue("")
+    setCategory("")
+  }
+
   return (
     <div className='todo-form'>
         <h2>Criar tarefa:</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type="text" placeholder='Digite o titulo'
-             />
-             <select>
+            value={value}
+             onChange={(e) => setValue(e.target.value)}/>
+             <select onChange={(e) => setCategory(e.target.value)} value={category}>
                 <option value="">Selecione uma categoria</option>
                 <option value="Trabalho">Trabalho</option>
                 <option value="Pessoal">Pessoal</option>
